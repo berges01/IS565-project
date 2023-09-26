cacaf9a2a566680ea6066308378ca7c54fef0cd6
Erik Berges <erik.t.berges@gmail.com>
2023-09-26 16:12:20 -0600

    updated to account for beta and other releases

--- chrome_vulnerabilities.py
+++ chrome_vulnerabilities.py
@@ -10,7 +10,7 @@ def is_chrome_installed():
         version = output.decode('utf-8').strip()
         revised_version = version.replace("Version=", "")
         print(revised_version)
-        if(revised_version == '117.0.5938.92'):
+        if(revised_version[:3] == '117' or revised_version[:3] == '118'):
             print("You are running the latest version of Google Chrome")
         else:
             print("You are running an outdated version of Google Chrome and you may be vulnerable")
@@ -23,19 +23,23 @@ def possible_vulnerabilities():
     output = subprocess.check_output(r'wmic datafile where name="C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" get Version /value',shell=True)
     version = output.decode('utf-8').strip()
     revised_version = version.replace("Version=", "")
-    if revised_version == '117.0.5938.92':
+    if(revised_version[:3] == '117' or revised_version[:3] == '118'):
         return True
-    elif revised_version == '114.0.5735.90':
+    
+    elif revised_version[:3] == '116' or revised_version[:3] == '115':
+        print("Your version of Google Chrome is out of data and may contain vulnerabilites")
+
+    elif revised_version[:3] == '114':
         print("Your version of Google Chrome is 4 months out of date and contains these vulnerabilities:")
         f = open('114.csv', 'r')
         print(f.read())
         f.close()
-    elif revised_version == '113.0.5672.126':
+    elif revised_version[:3] == '113':
         print("Your version of Google Chrome is 4 and a half months out of date and contains these vulnerabilities:")
         f = open('113.csv', 'r')
         print(f.read())
         f.close()
-    elif revised_version == '112.0.5615.49':
+    elif revised_version[:3] == '112':
         print("Your version of Google Chrome is 6 months out of date and contains these vulnerabilities:")
         f = open('112.csv', 'r')
         print(f.read())
