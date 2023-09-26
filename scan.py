0384fa84e082e1b86d60a22474027f4ce778eaf0
Erik Berges <erik.t.berges@gmail.com>
2023-09-26 11:36:17 -0600

    more changes

--- scan.py
+++ scan.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 
 def chrome_data():
+    #file path will need to be changed to detect username and better filepath
     file = open(r"D:\results.txt", "w+")
     chrome_file_path = "C:\Program Files\Google\Chrome"
     
@@ -9,21 +10,23 @@ def chrome_data():
         output = subprocess.check_output(r'wmic datafile where name="C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" get Version /value',shell=True)
         file.write("Google Chrome is running the following version: \n")
         file.write(output.decode('utf-8').strip())
-        #print("Google Chrome is running the following version:")
-        #print(print(output.decode('utf-8').strip()))
         return True
     else:
-        file.write("Goolge Chrome is not installed")
-        #print("Goolge Chrome is not installed")
+        file.write("Goolge Chrome is not installed \n")
         return False
     file.close()
     
 def firewall_data():
-    print("Here is the current status of your firewall profiles:")
-    subprocess.check_call('netsh advfirewall show domainprofile state')
-    subprocess.check_call('netsh advfirewall show privateprofile state')
-    subprocess.check_call('netsh advfirewall show publicprofile state')
-    print("if any of these profiles are set to 'OFF'")
+    file = open(r"D:\results.txt", "w+")
+    file.write("Here is the current status of your firewall profiles: \n")
+    domain_profile = subprocess.check_call('netsh advfirewall show domainprofile state')
+    file.write(str(domain_profile) + "\n")
+    private_profile = str(subprocess.check_call('netsh advfirewall show privateprofile state'))
+    file.write(private_profile + "\n")
+    public_profile = str(subprocess.check_call('netsh advfirewall show publicprofile state'))
+    file.write(public_profile + "\n")
+    file.write("If any of these profiles are set to 'OFF' you may be vulnerable to an attack")
+    file.write("To remidiate this __________________________________")
 
 
 
